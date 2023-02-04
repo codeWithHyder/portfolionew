1654e738797b0f79a07b9be4ed601aa2fef95c9d
Your Name <hyderali400@gmail.com>
2023-02-04 21:36:36 +0530

    fixed duplicay in css file

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function displayPopUp(){
  pop_up_menu.style.display="block";
  pop_up_menu.style.right=0;
  pop_up_menu.style.top=0;
- pop_up_menu.style.transition="ease-in-out 0.5s";
+ pop_up_menu.style.transition="ease-in-out 1s";
  pop_up_menu.style.zIndex=1;
 }
 
 function closePopUp(){
   let ele=document.getElementById("popup-menu-id");
-  ele.style.display="none";
+ 
   ele.style.right="-23em";
   ele.style.display.top=0;
-  ele.style.transition="0.5s ease-in-out";
-  ele.style.zIndex=2;
+  ele.style.transition="ease-in-out 0.5s";
+   ele.style.display="none";
 }
