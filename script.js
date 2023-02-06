e1a9902a1041ee8eb15ccf3d782723cd97885c1b
Your Name <hyderali400@gmail.com>
2023-02-06 13:32:45 +0530

    updated script and css files

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
+// Open mobile popup menu
 function displayPopUp(){
- let pop_up_menu= document.getElementById("popup-menu-id");
+ const pop_up_menu=document.querySelector("#popup-menu-id");
  pop_up_menu.style.display="block";
  pop_up_menu.style.right=0;
- pop_up_menu.style.top=0;
- pop_up_menu.style.transition="ease-in-out 1s";
- pop_up_menu.style.zIndex=1;
 }
 
+// close mobile popup menu
 function closePopUp(){
-  let ele=document.getElementById("popup-menu-id");
- 
-  ele.style.right="-23em";
-  ele.style.display.top=0;
-  ele.style.transition="ease-in-out 0.5s";
-   ele.style.display="none";
+  const pop_up_menu=document.querySelector("#popup-menu-id");
+  pop_up_menu.style.right='-23em';
+  pop_up_menu.style.display="none";
 }
+
+//popup window will go here for project details
